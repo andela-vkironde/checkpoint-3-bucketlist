b49976cc078ed5213abf0a4bf9c2130b30a46ae0
andela-vkironde <victor.kironde@andela.com>
2016-12-13 14:27:31 +0300

    ft(models): add bucketlist model

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -1,4 +1,5 @@
 class BucketList < ApplicationRecord
+  validates_presence_of :name
   belongs_to :user
   has_many :items
 end
--- spec/models/bucket_list_spec.rb
+++ spec/models/bucket_list_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BucketList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :name}
+  it { is_expected.to have_many :items}
+  it { is_expected.to belong_to :user}
 end
