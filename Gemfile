62a20b28b5ba1d3c027ed4d9a5951ccbba4b1519
andela-vkironde <victor.kironde@andela.com>
2016-12-13 14:50:04 +0300

    ft(models): add user model

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'puma', '~> 3.0'
 gem 'pg'
+gem 'bcrypt'
 
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length:{minimum: 6}
+  validates :password_confirmation,
+            presence: true
+  validates :email,
+            presence:true,
+            uniqueness: true,
+            format:{with: VALID_EMAIL}
+  has_secure_password
+
+
   has_many :bucket_lists
 end
