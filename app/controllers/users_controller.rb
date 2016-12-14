a1836a4cfda7cec7a50aa59fde002768c1417c50
andela-vkironde <victor.kironde@andela.com>
2016-12-14 23:59:52 +0300

    ft(controllers): add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      token = Authentication.new(user.email, user.password).call
+      response = { message: Messages.signup_successful,
+                   token: token }
+      json_response(response, :created)
+    else
+      json_response({ message: Messages.signup_failed }, :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+                  :first_name,
+                  :last_name,
+                  :email,
+                  :password,
+                  :password_confirmation
+                  )
+  end
+end
