9cc055a46767b567277a27d487bfb45d4142b4ad
andela-vkironde <victor.kironde@andela.com>
2016-12-15 12:09:49 +0300

    bg(): fix broken bucketlist item association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215074417) do
+ActiveRecord::Schema.define(version: 20161215085219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bucket_lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "bucketlists", force: :cascade do |t|
     t.string   "name"
@@ -26,9 +33,9 @@ ActiveRecord::Schema.define(version: 20161215074417) do
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.boolean  "done"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "bucket_list_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "bucketlist_id"
   end
 
   create_table "tokens", force: :cascade do |t|
@@ -48,8 +55,7 @@ ActiveRecord::Schema.define(version: 20161215074417) do
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "bucket_lists", "users"
   add_foreign_key "bucketlists", "users"
-  add_foreign_key "items", "bucket_lists"
+  add_foreign_key "items", "bucketlists"
   add_foreign_key "tokens", "users"
 end
