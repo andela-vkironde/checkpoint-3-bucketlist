c48a03acd20359b5df9f1b048581e81314ea05ba
andela-vkironde <victor.kironde@andela.com>
2016-12-15 12:43:17 +0300

    ft(controllers): namespace bucketlist controller

--- app/controllers/api/v1/bucketlists_controller.rb
+++ app/controllers/api/v1/bucketlists_controller.rb
@@ -1,14 +1,18 @@
-class BucketListsController < ApplicationController
-  def index
-  end
+module API
+  module V1
+    class BucketListsController < ApplicationController
+      def index
+      end
 
-  def create
-  end
+      def create
+      end
 
-  def destroy
-  end
+      def destroy
+      end
 
-  def update
-  end
+      def update
+      end
 
+    end
+  end
 end
