2405015cdc87740cbc503a305cad67cecaa5b195
andela-vkironde <victor.kironde@andela.com>
2016-12-19 19:59:53 +0300

    ch(models): refactor migrations

--- db/migrate/20161205091903_create_items.rb
+++ db/migrate/20161205091903_create_items.rb
@@ -3,7 +3,6 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.string :name
       t.boolean :done, default: false
-      t.references :bucket_lists, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20161215085219_add_bucketlist_item_association.rb
+++ db/migrate/20161215085219_add_bucketlist_item_association.rb
@@ -1,6 +1,6 @@
 class AddBucketlistItemAssociation < ActiveRecord::Migration[5.0]
   def change
-    rename_column :items, :bucket_list_id, :bucketlist_id
+    add_column :items, :bucketlist_id, :integer
     add_foreign_key :items, :bucketlists, column: :bucketlist_id, primary_key: "id"
   end
 end
