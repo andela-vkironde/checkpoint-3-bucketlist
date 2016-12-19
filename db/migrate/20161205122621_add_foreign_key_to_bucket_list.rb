ec6e83081b2085410c60154207225530072eaef5
andela-vkironde <victor.kironde@andela.com>
2016-12-19 19:16:23 +0300

    bg(models): fix failing migrations

--- db/migrate/20161205122621_add_foreign_key_to_bucket_list.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToBucketList < ActiveRecord::Migration[5.0]
-  def change
-    add_column :bucket_lists, :user_id, :integer
-    add_foreign_key :bucket_lists, :users, column: :user_id, primary_key: "id"
-  end
-end
