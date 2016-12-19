7e4556d79069b6d5bfb4dea67934b9838575cbf0
andela-vkironde <victor.kironde@andela.com>
2016-12-19 13:05:20 +0300

    ft(controllers): add bucketlist index action

--- app/controllers/api/v1/bucketlists_controller.rb
+++ app/controllers/api/v1/bucketlists_controller.rb
@@ -1,18 +1,34 @@
 module API
   module V1
-    class BucketListsController < ApplicationController
+    class BucketlistsController < ApplicationController
+      before_action :user_bucketlists, only: [:index]
+
       def index
+        queried_bucketlists = search || paginate_only
+        json_response(queried_bucketlists)
       end
 
-      def create
+      private
+
+      def bucketlist_params
+        params.permit(:name, :user_id)
       end
 
-      def destroy
+      def search
+        if params[:q] && @bucketlists
+          @bucketlists
+            .search(params[:q])
+            .paginate(params[:limit], params[:page])
+        end
       end
 
-      def update
+      def paginate_only
+        @bucketlists.paginate(params[:limit], params[:page]) if @bucketlists
       end
 
+      def user_bucketlists
+        @bucketlists = @current_user.bucketlists
+      end
     end
   end
 end
