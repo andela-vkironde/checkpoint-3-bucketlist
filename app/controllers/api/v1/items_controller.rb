ac24dc5034ba7fbfe0fd6feb0fc92501802b621b
andela-vkironde <victor.kironde@andela.com>
2016-12-19 15:03:14 +0300

    ft(controllers): add item index action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,13 +1,10 @@
 module API
   module V1
     class ItemsController < ApplicationController
+      before_action :bucketlist_items, only: [:index]
 
       def index
-
-      end
-
-      def create
-
+        json_response(paginate_items)
       end
 
       def update
@@ -26,6 +23,18 @@ module API
 
       def item_params
         params.permit(:name, :bucketlist_id, :done)
+      end
+
+      def paginate_items
+        @items.paginate(params[:limit], params[:page]) if @items
+      end
+
+      def bucketlist_items
+        @items = item_bucketlist.items if item_bucketlist
+      end
+
+      def item_bucketlist
+        @current_user.bucketlists.find_by(id: params[:bucketlist_id])
       end
     end
   end
