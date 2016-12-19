be6aaad92217fdd08010578f08b713caa3c839c2
andela-vkironde <victor.kironde@andela.com>
2016-12-19 11:51:56 +0300

    ft(models): add search to bucketlist model

--- app/models/bucketlist.rb
+++ app/models/bucketlist.rb
@@ -2,4 +2,15 @@ class Bucketlist < ApplicationRecord
   validates_presence_of :name
   belongs_to :user
   has_many :items
+
+  scope(:search, lambda do |name|
+    bucketlists = where("name ILIKE ?", "%#{name}%")
+    if bucketlists.blank?
+      raise(
+        ActiveRecord::RecordNotFound, Messages.not_found(name)
+      )
+    end
+    bucketlists
+  end)
+
 end
