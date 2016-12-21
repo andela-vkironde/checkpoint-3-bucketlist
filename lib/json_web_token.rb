2fe90f74e4edbf24c73d75a650bd63055d56b788
andela-vkironde <victor.kironde@andela.com>
2016-12-21 10:33:17 +0300

    ch(): refactor jwt

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -7,8 +7,10 @@ class JsonWebToken
   def self.decode(token)
     body = JWT.decode(token, Rails.application.secrets.secret_key_base).first
     HashWithIndifferentAccess.new body
-  rescue JWT::ExpiredSignature => e
-    @current_user.tokens.create!(token: token)
-    raise ExceptionHandler::ExpiredToken, e.message
+  rescue JWT::ExpiredSignature
+    raise ExceptionHandler::NotAuthorized, Messages.expired_token
+
+  rescue JWT::VerificationError
+    raise ExceptionHandler::NotAuthorized, Messages.invalid_token
   end
 end
