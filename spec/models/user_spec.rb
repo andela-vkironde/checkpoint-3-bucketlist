e6e4643102a09da25e5bfda4a8d7363624eb3106
andela-vkironde <victor.kironde@andela.com>
2017-01-05 09:46:05 +0300

    ft(tests): test user full name method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :password }
   it { is_expected.to have_many :bucketlists }
   it { is_expected.to have_many :tokens }
+
+  describe "#full_name" do
+    let(:user) { create :user }
+
+    it "returns the full name of a user" do
+      expect(user.full_name).to eq "#{user.first_name} #{user.last_name}"
+    end
+  end
 end
