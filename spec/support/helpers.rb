a50654c41ff6168892a3f47f904351ce79b72989
andela-vkironde <victor.kironde@andela.com>
2017-01-05 13:14:19 +0300

    ft(spec): add invalid tokens helper spec

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -11,6 +11,10 @@ module Helpers
     JsonWebToken.encode({ user_id: user_id }, (Time.now.to_i - 10))
   end
 
+  def invalid_tokens(user_id)
+    JsonWebToken.encode({ user_id: user_id } )
+  end
+
   def valid_headers(user_id)
     headers.merge("Authorization" => token(user_id))
   end
